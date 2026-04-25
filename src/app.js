6cc5b2f08d938bef53314d7fed72859cf961876a
JhersonApaza <apazajherson8@gmail.com>
2026-04-24 22:45:49 -0500

    cambios por ahora xd

--- src/app.js
+++ src/app.js
@@ -33,12 +33,11 @@ app.get('/', (req, res) => res.redirect('/login'));
 
 app.use('/', authRoutes);
 app.use('/', estudianteRoutes);
-
 // RUTA TEMPORAL — borrar después
 app.get('/fix-correo', (req, res) => {
   const db = require('./config/db');
   db.query(
-    "UPDATE supervisores SET correo = 'jherson8@gmail.com' WHERE correo = 'supervisor@empresa.com'",
+    "UPDATE supervisores SET correo = 'apazajherson8@gmail.com' WHERE correo = 'jherson8@gmail.com'",
     (err, result) => {
       if (err) return res.json({ error: err.message });
       res.json({ ok: true, filas_actualizadas: result.affectedRows });
